6eda1aa2a10740540337b0608961291e9ae4dd9f
gorkemunuvar <a.gorkemunuvar@gmail.com>
2021-10-19 01:37:55 +0300

    create fake news endpoint

--- resources/news.py
+++ resources/news.py
@@ -2,18 +2,23 @@ from flask import request, abort
 from flask_restful import Resource
 from marshmallow import Schema, fields
 
+from test_data.news import test_news_data
+
+
 class NewsQuerySchema(Schema):
     page = fields.Str()
     per_page = fields.Str()
 
+
 schema = NewsQuerySchema()
+
 
 class News(Resource):
     @classmethod
     def get(self):
         errors = schema.validate(request.args)
-        
+
         if errors:
             abort(400, str(errors))
 
-        return {'todo_id': request.args}
+        return {'news': test_news_data}, 200
---
+++ test_data/news.py
@@ -0,0 +1,65 @@
+_title = '12 Ekim\'de randevusu olan hastalarımızın dikkatine'
+_description = 'Hastanemizde yapılacak olan zorunlu tadilat çalışmalarından dolayı 12 Ekimde randevusu olan hastalarımızın randevularını ertelemesi gerekmektedir.'
+
+test_news_data = [
+    {
+        'title': _title,
+        'description': _description,
+        'date': '19.12.2021',
+        'image_url': 'http://localhost:5000/static/images/news/aigerim1.png'
+    },
+    {
+        'title': _title,
+        'description': _description,
+        'date': '19.12.2021',
+        'image_url': 'http://localhost:5000/static/images/news/aigerim2.png'
+    },
+    {
+        'title': _title,
+        'description': _description,
+        'date': '19.12.2021',
+        'image_url': 'http://localhost:5000/static/images/news/aigerim3.png'
+    },
+    {
+        'title': _title,
+        'description': _description,
+        'date': '19.12.2021',
+        'image_url': 'http://localhost:5000/static/images/news/aigerim4.png'
+    },
+    {
+        'title': _title,
+        'description': _description,
+        'date': '19.12.2021',
+        'image_url': 'http://localhost:5000/static/images/news/aigerim5.png'
+    },
+    {
+        'title': _title,
+        'description': _description,
+        'date': '19.12.2021',
+        'image_url': 'http://localhost:5000/static/images/news/aigerim6.png'
+    },
+    {
+        'title': _title,
+        'description': _description,
+        'date': '19.12.2021',
+        'image_url': 'http://localhost:5000/static/images/news/aigerim7.png'
+    },
+    {
+        'title': _title,
+        'description': _description,
+        'date': '19.12.2021',
+        'image_url': 'http://localhost:5000/static/images/news/aigerim8.png'
+    },
+    {
+        'title': _title,
+        'description': _description,
+        'date': '19.12.2021',
+        'image_url': 'http://localhost:5000/static/images/news/aigerim9.png'
+    },
+    {
+        'title': _title,
+        'description': _description,
+        'date': '19.12.2021',
+        'image_url': 'http://localhost:5000/static/images/news/aigerim10.png'
+    },
+]
